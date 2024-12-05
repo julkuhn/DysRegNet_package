ecf21b2be52e1b12dd11e645815201fce600a053
julkuhn <144685155+julkuhn@users.noreply.github.com>
2024-12-05 10:53:24 +0100

    first adaption save models

--- src/dysregnet/functions.py
+++ src/dysregnet/functions.py
@@ -6,7 +6,11 @@ import numpy as np
 from scipy import stats
 import statsmodels.stats.multitest as mt
 import statsmodels.api as sm
-
+#_____________
+import pickle
+import joblib
+import time
+#_____________
 
 def process_data(data):
        
@@ -81,6 +85,11 @@ def dyregnet_model(data):
         
         # prepare data
         
+        #_______
+        average_pickle = []
+        average_joblib = []
+        #_______
+        
         if data.cov_df is not None:
             control=pd.merge(data.cov_df.loc[data.control],data.expr, left_index=True, right_index=True).drop_duplicates()
             case=pd.merge(data.cov_df.loc[data.case],data.expr, left_index=True, right_index=True).drop_duplicates()
@@ -109,8 +118,40 @@ def dyregnet_model(data):
 
                         # fit the model
                         model = sm.OLS(y_train, x_train)
-                        results = model.fit()
-                        
+                        results = model.fit() # TODO interessant
+
+                        # __________________________________________Save the model
+                        "start our code"
+
+                        # Measure time for pickle
+                        start_pickle = time.time()
+                        with open("ols_model.pkl", "wb") as file:
+                            pickle.dump(model, file)
+                        with open("ols_model.pkl", "rb") as file:
+                            results_pickle = pickle.load(file)
+                        end_pickle = time.time()
+                        average_pickle.append(end_pickle - start_pickle)
+                        #print("Pickle takes ", end_pickle - start_pickle, "seconds")
+
+                        # Measure time for joblib
+                        start_joblib = time.time()
+                        joblib.dump(model, "ols_model.joblib")
+                        results_joblib = joblib.load("ols_model.joblib")
+                        end_joblib = time.time()
+                        average_joblib.append(end_joblib - start_joblib)
+
+                        #print("Joblib takes ", end_joblib - start_joblib, "seconds")
+
+                    
+                        # Load the model 
+                        loaded_model = joblib.load("ols_model.joblib") 
+                        # Access model parameters 
+                        #print("Coefficients:", loaded_model.params) 
+                        #print("R-squared:", loaded_model.rsquared)
+
+                        #results = results_pickle.params
+                        #results = results_joblib.params
+                        # _____________________________________________
                         model_stats[edge] = [results.rsquared] + list(results.params.values) + list(results.pvalues.values)
                         
                         # get residuals of control
@@ -186,7 +227,11 @@ def dyregnet_model(data):
 
                         edges[edge] = np.round(zscore, 1)
 
-                        
+        #______
+        print("Average pickle: ", np.average(average_pickle))
+        print("Average joblib: ", np.average(average_joblib))
+
+        #______
                     
         results = pd.DataFrame.from_dict(edges)
         results = results.set_index('patient id')
